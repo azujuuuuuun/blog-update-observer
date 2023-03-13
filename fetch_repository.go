deaee005b8cade8005fe0a5ab654d3565da2699a
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2023-03-13 22:50:02 +0900

    Use repository

--- fetch_repository.go
+++ fetch_repository.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"encoding/xml"
@@ -13,6 +14,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
 
 func FetchOldFeed() (Feed, error) {
@@ -71,4 +73,35 @@ func FetchLatestFeed() (Feed, error) {
 		return Feed{}, err
 	}
 	return feed, nil
+}
+
+func UploadFeedFile(feed Feed) error {
+	b, err := json.MarshalIndent(feed, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	endpoint := os.Getenv("STORAGE_API_ENDPOINT")
+	accessKeyID := os.Getenv("STORAGE_ACCESS_KEY_ID")
+	accessKeySecret := os.Getenv("STORAGE_SECRET_ACCESS_KEY")
+	bucket := os.Getenv("GCS_BUCKET_NAME")
+	object := os.Getenv("GCS_OBJECT_NAME")
+
+	sess := session.Must(session.NewSession(&aws.Config{
+		Region:           aws.String("auto"),
+		Endpoint:         aws.String(endpoint),
+		Credentials:      credentials.NewStaticCredentials(accessKeyID, accessKeySecret, ""),
+		S3ForcePathStyle: aws.Bool(true),
+	}))
+	uploader := s3manager.NewUploader(sess)
+
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, time.Second*10)
+	defer cancel()
+
+	_, err = uploader.UploadWithContext(ctx, &s3manager.UploadInput{Bucket: &bucket, Key: &object, Body: bytes.NewReader(b)})
+	if err != nil {
+		return err
+	}
+	return nil
 }

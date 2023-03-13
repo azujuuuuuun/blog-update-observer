bcbff29eaa4ae528da6348dc7385eba73a9e4ce9
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2023-03-13 23:01:40 +0900

    Change inline struct definition

--- feed.go
+++ feed.go
@@ -11,28 +11,32 @@ type Author struct {
 	Name string `xml:"name" json:"name"`
 }
 
+type Category struct {
+	XMLName xml.Name `xml:"category" json:"-"`
+	Term    string   `xml:"term,attr" json:"term"`
+	Label   string   `xml:"label,attr" json:"label"`
+}
+
+type Entry struct {
+	Title     string   `xml:"title" json:"title"`
+	Link      []Link   `xml:"link" json:"link"`
+	Id        string   `xml:"id" json:"id"`
+	Published string   `xml:"published" json:"published"`
+	Updated   string   `xml:"updated" json:"updated"`
+	Summary   string   `xml:"summary" json:"summary"`
+	Content   string   `xml:"content" json:"content"`
+	Category  Category `xml:"category" json:"category"`
+	Author    Author   `xml:"author" json:"author"`
+}
+
 type Feed struct {
-	Title    string `xml:"title" json:"title"`
-	Subtitle string `xml:"subtitle" json:"subTitle"`
-	Link     Link   `xml:"link" json:"link"`
-	Updated  string `xml:"updated" json:"updated"`
-	Author   Author `xml:"author" json:"author"`
-	Id       string `xml:"id" json:"id"`
-	Entry    []struct {
-		Title     string `xml:"title" json:"title"`
-		Link      []Link `xml:"link" json:"link"`
-		Id        string `xml:"id" json:"id"`
-		Published string `xml:"published" json:"published"`
-		Updated   string `xml:"updated" json:"updated"`
-		Summary   string `xml:"summary" json:"summary"`
-		Content   string `xml:"content" json:"content"`
-		Category  struct {
-			XMLName xml.Name `xml:"category" json:"-"`
-			Term    string   `xml:"term,attr" json:"term"`
-			Label   string   `xml:"label,attr" json:"label"`
-		} `xml:"category" json:"category"`
-		Author Author `xml:"author" json:"author"`
-	} `xml:"entry" json:"entry"`
+	Title    string  `xml:"title" json:"title"`
+	Subtitle string  `xml:"subtitle" json:"subTitle"`
+	Link     Link    `xml:"link" json:"link"`
+	Updated  string  `xml:"updated" json:"updated"`
+	Author   Author  `xml:"author" json:"author"`
+	Id       string  `xml:"id" json:"id"`
+	Entry    []Entry `xml:"entry" json:"entry"`
 }
 
 func IsBlogUpdated(of Feed, nf Feed) bool {
